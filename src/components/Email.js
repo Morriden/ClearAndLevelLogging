66cbb458ad943a8668b93dde65f42352bf7d1acf
Erik Ford <ekford18@gmail.com>
2021-05-17 14:32:22 -0700

    changed contact areas for client requests. Correct mobile look as well

--- src/components/Email.js
+++ src/components/Email.js
@@ -7,76 +7,58 @@ const Email = () => {
     return (
         <EmailContainer id="contactusPage">
             <EmailContent>
+            <ContactInformation>
+                <div2>
+                    <h1>Owner's and Operators</h1>
+                        <p>Dave Forfar</p>
+                        <p>Willene Forfar</p>
+                </div2>
+                <div3>
+                    <h1>Call Us</h1>
+                    <p>360-921-4986</p>
+                </div3>
+                <div4>
+                    <h1>Mailing Address</h1>
+                    <p>PO Box 462
+                        <br/>
+                    Amboy, WA 98601
+                    </p>
+                </div4>
+            </ContactInformation>
         <form action="#">
         <h1>Contact Dave (What should go here?)</h1>
-            <FormWrapContactInfo>
-                <label htmlFor="fullname"></label>
-                    <input type="text" placeholder="Full Name"id="fullname" />
-                <label htmlFor="phonenumber"></label>
-                    <input type="number" placeholder="Phone Number" id="phonenumber" />
-                    </FormWrapContactInfo>
-                    <FormWrapEmailInfo>
-                <label htmlFor="email"></label>
-                    <input type="email" placeholder="Email Address" id="email" />
-                <label htmlFor="subject"></label>
-                    <input type="text" placeholder="Subject" id="subject" />
-                
-                <label htmlFor="message"></label>
-                    <textarea type="text" id="message" placeholder="Message" className="message"/>
-                    </FormWrapEmailInfo>
+                <FormWrapContactInfo>
+                    <label htmlFor="fullname"></label>
+                        <input type="text" placeholder="Full Name"id="fullname" />
+                    <label htmlFor="phonenumber"></label>
+                        <input type="number" placeholder="Phone Number" id="phonenumber" />
+                </FormWrapContactInfo>
+                <FormWrapEmailInfo>
+                    <label htmlFor="email"></label>
+                        <input type="email" placeholder="Email Address" id="email" />
+                    <label htmlFor="subject"></label>
+                        <input type="text" placeholder="Subject" id="subject" />
+                    <label htmlFor="message"></label>
+                        <textarea type="text" id="message" placeholder="Message" className="message"/>
+                </FormWrapEmailInfo>
                 
                 <Button as="button" type="submit" primary="true" round="true" css={`
-                height: 48px;
-                margin: auto;
-                @media screen and (max-width: 768px) {
+                    height: 48px;
                     margin: auto;
-                    width: 20%
-                    min-width: 250px; 
-                }
-                @media screen and (max-width: 400px) {
-                    margin: auto;
-                    width: 20%;
-                    min-width: 250px;
-                }
-                `}>
-                    Contact Us
+                    @media screen and (max-width: 768px) {
+                        margin: auto;
+                        width: 20%
+                        min-width: 250px; 
+                    }
+                    @media screen and (max-width: 400px) {
+                        margin: auto;
+                        width: 20%;
+                        min-width: 250px;
+                    }
+                    `}>
+                        Contact Us
                 </Button>
             
-        </form>
-        <form>
-        <h1>Contact Willene(what should go here?)</h1>
-        <FormWrapContactInfo>
-                <label htmlFor="fullname"></label>
-                    <input type="text" placeholder="Full Name"id="fullname" />
-                <label htmlFor="phonenumber"></label>
-                    <input type="number" placeholder="Phone Number" id="phonenumber" />
-                    </FormWrapContactInfo>
-                    <FormWrapEmailInfo>
-                <label htmlFor="email"></label>
-                    <input type="email" placeholder="Email Address" id="email" />
-                <label htmlFor="subject"></label>
-                    <input type="text" placeholder="Subject" id="subject" />
-                
-                <label htmlFor="message"></label>
-                    <textarea type="text" id="message" placeholder="Message" className="message"/>
-                    </FormWrapEmailInfo>
-                
-                <Button as="button" type="submit" primary="true" round="true" css={`
-                height: 48px;
-                margin: auto;
-                @media screen and (max-width: 768px) {
-                    margin: auto;
-                    width: 20%
-                    min-width: 250px; 
-                }
-                @media screen and (max-width: 400px) {
-                    margin: auto;
-                    width: 20%;
-                    min-width: 250px;
-                }
-                `}>
-                    Contact Us
-                </Button>
         </form>
         </EmailContent>
         </EmailContainer>
@@ -112,9 +94,8 @@ const EmailContainer = styled.div`
 
 const EmailContent = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     align-items: center;
-    justify-content: space-between;
     width: 100%;
 
     h1 {
@@ -125,10 +106,10 @@ const EmailContent = styled.div`
     }
 
     p {
-        text-align: center;
+        /* text-align: center; */
         font-size: clamp(1rem, 2.5vw, 1.5rem);
         padding: 0 1rem;
-        margin-bottom: 2rem;
+        margin-bottom: 1rem;
     }
 
     form {
@@ -223,5 +204,31 @@ padding: 10px;
             width: 100%;
             margin-right: 0;
         }
+    }
+`
+
+const ContactInformation = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin-bottom: 8rem;
+    width: 1100px;
+
+div2, div3, div4 {
+        display: flex;
+        flex-direction: column;
+    }
+
+h1 {
+    text-align: start;
+    text-decoration: underline;
+}
+
+@media screen and (max-width: 768px) {
+     display: flex;
+     flex-direction: column;
+     padding-top: 2rem;
+     margin-bottom: 2rem;
+     width: auto;
     }
 `
